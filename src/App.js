28a6128e7a33ef6411a6cec0b2039296884bde00
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-13 10:28:58 +0200

    styles and structure added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import "./App.css";
+import Navbar from "./Navbar"
+import Home from "./Home"
 
 function App() {
   return (
     <div className="App">
-      <h1>App component</h1>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
