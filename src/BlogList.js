bcb2dee83b391853c2b13f8b944271d640ded809
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-17 07:42:56 +0200

    Fetching data from json file

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,16 +1,15 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
-      {blogs.map(blog => (
+      {blogs.map((blog) => (
         <div key={blog.id} className="blog-preview">
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default BlogList
+export default BlogList;
