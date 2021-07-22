18608bf91b79f9c3ba3296c9ebeedcac75945906
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-22 16:38:56 +0200

    added redirect

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +18,8 @@ const Create = () => {
       body: JSON.stringify(blog),
     }).then(() => {
       console.log("new blog added");
+      setIsPending(false);
+      history.push("/");
     });
   };
 
