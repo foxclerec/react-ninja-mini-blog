365ef5a5964812f62bbf0224f338ab7dccff62a7
Anatolii Skoryk <anatolii.skoryk@gmail.com>
2021-07-22 10:12:26 +0200

    added POST request

--- src/Create.js
+++ src/Create.js
@@ -7,14 +7,20 @@ const Create = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     const blog = { title, body, author };
-    console.log(blog);
+
+    fetch("http://localhost:8000/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added");
+    });
   };
 
   return (
     <div className="create">
-      <h2>Add a new blog</h2>
+      <h2>Add a New Blog</h2>
       <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
@@ -23,21 +29,18 @@ const Create = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-
         <label>Blog body:</label>
         <textarea
           required
           value={body}
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
-
         <label>Blog author:</label>
         <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-
-        <button>Add blog</button>
+        <button>Add Blog</button>
       </form>
     </div>
   );
